ec99676f0c92fc87c43ec9d77c39ec5af92094c7
Utsav <liveasia66@gmail.com>
2021-10-27 18:35:20 -0500

    firstscreen

---
+++ FirstScreen.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import Assets from "./components/Assets";
+
+/**
+ *
+ * This is the first splash scree showing pickbins logout. Timout for showing this screen is set to 2s.
+ */
+const FirstScreen = ({ navigation }) => {
+  <View style={styles.container}>
+    <Text>FirstScreen</Text>
+  </View>;
+
+  // useEffect(() => {
+  //   console.log("once");
+  //   setTimeout(() => {
+  //     navigation.navigate("SignInScreen");
+  //   }, 2000);
+  // }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <View>
+        <Assets />
+      </View>
+      <Text style={styles.innertext}>Pick Bins</Text>
+    </View>
+  );
+};
+export default FirstScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innertext: {
+    flex: 0.2,
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#00A600",
+  },
+});
